4cf6c7c3eef3429ca539f6c0ce0e7db2f388eead
Nils <nils@opv.de>
2023-11-17 22:04:57 +0100

    added auto detection of correct rotation

--- cmd/main.go
+++ cmd/main.go
@@ -3,22 +3,101 @@ package main
 import (
 	"fmt"
 	"strings"
+	"os"
+	"bufio"
 )
 
+
+
 func main() {
-		text := "Nutella ohne Butter ist besser als Nutella mit Butter"
-		shift := 17
-		result := rot(text, alph, shift)
-		fmt.Printf("%s rotiert um %d ergibt %s\n", text, shift, result)
+	// func 1
+	// 1. Das programm bekommt einen Text und einen int um wie viel es rotieren soll
+	// 2. dann gibt es den rotierten Text aus
+	// func 2
+	// 3. bekommt einen rotierten Text denn es entschlüsseln muss mit hilfe der wordlist
+	// 4. ratiert den Text so lang wie das Alphabet ist 
+	// 5. vergleicht jedes wort mit der wordlist und zählt welche rotation am meisten matches bekommt
+	// 6. was die warscheinlichste übersetzung ist  
+
+	wordList := readWordList("german_word_list.txt")
+	wordMap := convertListToMap(wordList)
+	var hits = make([]int, len(alphabet))
+	text := readEncryptedTextFromUser()
+	text = strings.TrimSpace(text)
+	for shift := 0; shift < len(alphabet); shift++ {
+		result := rot(text, alphabet, shift)
+		hits[shift] = countWordsInLanguageMap(result, wordMap)
+	}
+	idx, maxHits := findMaxElementInArray(hits)
+	decryptedText := rot(text, alphabet, idx)
+	decryptedShfit := len(alphabet)-idx
+	fmt.Printf("Entschlüsselter Text:\n%s\nDer Text wurde um %d Stellen rotiert. Es gab %d Treffer in der Wortliste.\n", decryptedText, decryptedShfit, maxHits)
 }
 
-var alph = []string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+var alphabet = []string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
 
-// rot takes a string, an alphabet and a rotation (shift) value
-// returns the string rotated by rotation value on the alphabet
-// If alphabet is empty rot returns an empty string
-// If an character in the input text does not exists in the alphabet
-// the character is omitted in the result
+
+
+// readWordList reads file of words and gives back an arrays of strings
+func readWordList(filePath string ) []string {
+	words, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} 
+	return strings.Split(string(words), "\n")
+}
+
+// convertListToMap converts the word list to a map/table
+func convertListToMap(elements []string) map[string]bool {
+    elementMap := make(map[string]bool)
+
+    for _, s := range elements {  
+       elementMap[strings.ToLower(s)] = true
+    }
+    return elementMap
+}
+
+// readEncryptedTextFromUser asks for the rotated string from the user that wants to be correctly rotated
+func readEncryptedTextFromUser() string {
+	fmt.Println("Bitte einen verschlüsselteten Text eingeben")
+    reader := bufio.NewReader(os.Stdin)
+    line, err := reader.ReadString('\n')
+    if err != nil {
+        os.Exit(1)
+    }
+ 	return line
+}
+
+// countWordsInLanguageMap counts the words of every rotation by matches of the word list
+func countWordsInLanguageMap(rotatedText string, wordMap map[string]bool) int {
+	var counter int
+	for _, word := range strings.Split(rotatedText, " ") {
+		if wordMap[word] {
+			counter++
+		}
+
+	}
+	return counter
+}
+
+// findMaxElementInArray scans a given array for its highest value
+// returns the index and the value it was in
+func findMaxElementInArray(a []int) (int, int) {
+	var max int
+	var idx int
+	for i := 0; i < len(a); i++ {
+		if a[i] > max {
+			max = a[i]
+			idx = i
+		}
+	}
+	return idx, max
+}
+
+
+
+// rot it just rotates a string
 func rot(text string, alphabet []string, shift int) string {
 	if len(alphabet) == 0 {
 		return ""
@@ -44,3 +123,5 @@ func rot(text string, alphabet []string, shift int) string {
 	}
 	return result
 }
+
+
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -25,13 +25,38 @@ func TestRot(t *testing.T) {
 			{ testnumber: 8, alphabet: []string{}, text: "a", shift: 1, want: "",},
 			{ testnumber: 9, alphabet: latin_alphabet, text: "Nils", shift: 1, want: "ojmt",},
 			{ testnumber: 10, alphabet: latin_alphabet, text: "Nils", shift: 26, want: "nils",},
-			//{ testnumber: 11, alphabet: latin_alphabet, text: "Jörn", shift: 1, want: "k",},
-	}
+			{ testnumber: 11, alphabet: latin_alphabet, text: "Ich gehe morgen zum Zahnarzt", shift: 3, want: "lfk jhkh prujhq cxp cdkqducw",},
+	}		
 
 	for _, tc := range tests {
 		got := rot(tc.text, tc.alphabet, tc.shift)
 		if tc.want != got {
 			t.Fatalf("expected: %v, got: %v in test: %d", tc.want, got, tc.testnumber)
+		}
+	}
+}
+
+
+func TestFindMaxElementInArray(t *testing.T) {
+	type test struct {
+		testnumber int
+		a []int
+		wantIdx int
+		wantValue int
+	}
+
+	tests := []test{
+			{ testnumber: 1, a: []int{1,2,3}, wantIdx: 2, wantValue: 3},
+			{ testnumber: 2, a: []int{1,2,3,99,77,0,11}, wantIdx: 3, wantValue: 99},
+			{ testnumber: 3, a: []int{1}, wantIdx: 0, wantValue: 1},
+			{ testnumber: 4, a: []int{99,3,2,1,99}, wantIdx: 0, wantValue: 99},
+			{ testnumber: 5, a: []int{99,4,2,1}, wantIdx: 0, wantValue: 99},
+	}		
+
+	for _, tc := range tests {
+		gotIdx, gotValue := findMaxElementInArray(tc.a)
+		if tc.wantIdx != gotIdx || tc.wantValue != gotValue {
+			t.Fatalf("expected: idx %d val %d, got: idx %d val %d in test: %d", tc.wantIdx, tc.wantValue, gotIdx, gotValue, tc.testnumber)
 		}
 	}
 }
